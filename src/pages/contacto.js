d82873912b728df8ad0b1e42c819ddd66761058e
Rodrigo Milesi <rmilesi009@gmail.com>
2021-12-16 16:55:04 -0300

    ADD contact and FIX stock

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -177,7 +177,7 @@ const Contacto = ({ informacion }) => {
     const handleSubmit = e => {
         e.preventDefault()
         setLoading(true)
-        console.log(window.grecaptcha.getResponse(grecaptcha))
+        console.log(grecaptcha.getResponse("g-recaptcha"))
         if(from.billing_first_name === "" || from.email === "" || from.message === ""){
             setLoading(false)
             alert("Todos los campos son obligatorios")
